b3966e62b446e7a1fe5225f8384c80a69e6b0f72
Kibaek Kim <kibaek.kimm@gmail.com>
2023-01-16 01:30:00 +0900

    feat: button 테스트케이스 추가
    - 기존 테스트에 클릭 이후 테스트케이스 추가
    - 기존 테스트 삭제

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {
+test("button has correct initial color, and updates when clicked", () => {
   render(<App />);
 
   // find an element with a role of button and test of "Change of blue"
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   // expect the background color to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: "WDQWDWD" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the text to be "Change to red"
+  expect(colorButton).toHaveTextContent("Change to red");
 });
-
-test("button has correct initail text", () => {});
-
-test("button turns blue when clicked", () => {});
