e21d86dd8c3126cfa8ada00020f1f6149e96d5c3
Kibaek Kim <kibaek.kimm@gmail.com>
2023-01-16 01:00:45 +0900

    feat: `button has correct initial color` 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and test of "Change of blue"
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "WDQWDWD" });
 });
+
+test("button has correct initail text", () => {});
+
+test("button turns blue when clicked", () => {});
