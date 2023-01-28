cdfec4770e883d5e38e8752f9a936a468b6f8f81
Kibaek Kim <kibaek.kimm@gmail.com>
2023-01-28 14:06:08 +0900

    feat: Popover 인터렉션 테스트코드 추가

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -42,3 +42,28 @@ describe("SummaryForm component", () => {
     expect(button).toBeDisabled();
   });
 });
+
+describe("Popover", () => {
+  test("Popover responds to hover", async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when we mouse out
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
+  });
+});
