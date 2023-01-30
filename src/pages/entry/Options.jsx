e34b7bb050214d90c4516d448214483b9da9b6b4
Kibaek Kim <kibaek.kimm@gmail.com>
2023-01-31 02:22:33 +0900

    feat: Options, ScoopOption 컴포넌트 코딩

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((err) => {
+        //TODO: handle error response
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
