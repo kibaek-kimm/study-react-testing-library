6a602b70602c653c119e3c674f791bf7fa36d5fa
Kibaek Kim <kibaek.kimm@gmail.com>
2023-04-30 20:43:12 +0900

    feat: 테스트 시 Prodiver적용을 위해 wrapper 추가

--- src/pages/entry/tests/TotalUpdates.test.jsx
+++ src/pages/entry/tests/TotalUpdates.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import { OrderDetailsProvider } from "../../../contexts/OrderDetail";
 
 test("update scoop subtotal when scoop change", async () => {
   const user = userEvent.setup();
-  render(<Options optionType="scoops" />);
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
 
   // make sure total starts out at $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", {
